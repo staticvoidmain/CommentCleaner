8d907a320ab1304078fc86758183371ac4af9369
Ross Jennings <themissinglinq@gmail.com>
2013-03-21 19:10:56 -0500

    added src

---
+++ src/Tests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommentCleaner;
+
+namespace Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_When_Invalid_Argument_Specified_Writes_Help()
+        {
+			
+            Program p = new Program(
+        }
+
+        [TestMethod]
+        public void Main_When_Dir_Not_Specified_Writes_Help()
+        {
+
+        }
+    }
+}
